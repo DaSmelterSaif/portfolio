7416064a1cf9f357d1940152eec7c6b8e1954c0f
Saif Aldin Sunjaq <saifmarwan774@gmail.com>
2026-01-06 03:41:32 +0400

    hero section (desktop)

--- src/components/HeroSec.vue
+++ src/components/HeroSec.vue
@@ -12,18 +12,25 @@ export default {
 </script>
 
 <template>
+    <!-- TODO - Fix vertical alignment (especially when pressing F11) -->
     <GenericSec
-        class="flex flex-row relative -z-20 overflow-hidden bg-radial-[at_100%_0%] from-[#002133] from-15% to-bg-dark to-65% h-[calc(100vh-125px)]">
+        class="flex flex-row xll:px-56 relative -z-20 overflow-hidden bg-radial-[at_100%_0%] from-[#002133] from-15% to-bg-dark to-65% h-[calc(100vh-125px)]">
 
-        <div class="mt-24 w-124">
-            <div class="border-b-12 border-solid border-primary">
-                <h1 class="text-title text-text">Building The Web</h1>
+        <div class="flex-7"> <!-- bg-blue-300/30 -->
+            <div class="mt-24 max-w-124 2xl:ml-20">
+                <div class="border-b-12 border-solid border-primary pb-4">
+                    <h1 class="text-title text-text text-5xl 2xl:text-7xl leading-tight">Building The Web</h1>
+                </div>
+                <p class="text-body text-text-muted mt-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nullam
+                    id semper erat. Vestibulum ante ipsum.</p>
             </div>
-            <p class="text-body text-text-muted mt-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                id semper erat. Vestibulum ante ipsum.</p>
         </div>
 
-        <div class="bg-highlight w-97.75 h-139.25 mt-18 ml-72"></div>
+        <div class="flex justify-center items-start flex-10"> <!-- bg-amber-300/30 -->
+            <div class="bg-highlight w-97.75 aspect-391/557 xll:w-105 mt-18 ml-[10vw] 2xl:ml-[4vw]"></div>
+            <!-- 2xl:ml-[20vw] -->
+        </div>
 
         <p class="absolute -bottom-57 -left-37 -z-10 select-none quicksand-font text-[512px] text-[#002133]">
             &#60;&#47;&#62;
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -8,11 +8,11 @@ export default {
 
 <template>
     <nav class="flex flex-row w-full h-31.25 bg-bg-db-light text-text select-none">
-        <a href="#" class="text-nav-link w-58.75 flex justify-center items-center">LOGO</a>
-        <a href="#" class="text-nav-link flex-1 flex justify-center items-center">BTN1</a>
-        <a href="#" class="text-nav-link flex-1 flex justify-center items-center">BTN2</a>
-        <a href="#" class="text-nav-link flex-1 flex justify-center items-center">BTN3</a>
-        <a href="#" class="text-nav-link flex-1 flex justify-center items-center">BTN4</a>
+        <a href="#" class="text-nav-link w-58.75 flex justify-center items-center text-[40px]">LOGO</a>
+        <a href="#" class="text-nav-link flex-1 flex justify-center items-center text-[40px]">BTN1</a>
+        <a href="#" class="text-nav-link flex-1 flex justify-center items-center text-[40px]">BTN2</a>
+        <a href="#" class="text-nav-link flex-1 flex justify-center items-center text-[40px]">BTN3</a>
+        <a href="#" class="text-nav-link flex-1 flex justify-center items-center text-[40px]">BTN4</a>
     </nav>
 </template>
 
